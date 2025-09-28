f71930d83dccea68e19310893740e8dd948e2201
M <marilynduf@gmail.com>
2025-09-28 18:09:27 -0400

    Fix props drilling with Context API

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
+import { CartContext } from "../src/components/store/shopping-cart-context.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
 
 function App() {
@@ -67,23 +68,23 @@ function App() {
         });
     }
 
+    const ctxValue = {
+        items: shoppingCart.items,
+        addItemToCart: handleAddItemToCart,
+        updateCartItemsQty: handleUpdateCartItemQuantity,
+    };
+
     return (
-        <>
-            <Header
-                cart={shoppingCart}
-                onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-            />
+        <CartContext value={ctxValue}>
+            <Header />
             <Shop>
                 {DUMMY_PRODUCTS.map((product) => (
                     <li key={product.id}>
-                        <Product
-                            {...product}
-                            onAddToCart={handleAddItemToCart}
-                        />
+                        <Product {...product} />
                     </li>
                 ))}
             </Shop>
-        </>
+        </CartContext>
     );
 }
 
